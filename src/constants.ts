095fb9c9d8fd3276b8aa2a96e452e0a7de195ec8
BABAH GEE <61418505+babahgee@users.noreply.github.com>
2023-11-16 14:49:42 +0100

    Added the folder map structure, and some other AppData constants.

--- src/constants.ts
+++ src/constants.ts
@@ -21,3 +21,33 @@ export const SERVER_PORT: number = 8000;
 
 /** Private .env file. */
 export const DOTENV_FILE: string = path.join(ROOT_PATH, ".env");
+
+/** Directory of %appdata%, which might be undefined. */
+export const APPDATA_PATH: string | undefined = process.env.APPDATA;
+
+/** Name of the directory where the application data will be stored. */
+export const APPDATA_DIRECTORY_NAME: string = "fluent-youtube-downloader";
+
+export const APPDATA_DIRECTORY_STRUCTURE: { [K: string]: any } = {
+	"blob_storage": {},
+	"Cache": {
+		"Cache_Data": {
+			"f0933i": "<entry> index.ejs"
+		}
+	},
+	"Code Cache": {
+		"js": {},
+		"wasm": {}
+	},
+	"DawnCache": {},
+	"GPUCache": {},
+	"Local Storage": {
+		"levldb": {}
+	},
+	"Network": {},
+	"Session Storage": {},
+	"Application": {
+		"Settings.json": JSON.stringify({"window":{"resolution":{"width":600,"height":750},"display":{"theme":"fluent-light-purple"}},"path":{"appDataPath":null,"downloadPath":null,"ffmpegPath":null},"server":{"port":8000}}),
+		"History.json": "[]"
+	}
+};
