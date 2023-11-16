0763ceaaf1751a11ba945ab2491dfe4509b4fd2a
BABAH GEE <61418505+babahgee@users.noreply.github.com>
2023-11-16 14:48:39 +0100

    Added types for the entire application settings, including a fail object interface.

--- src/typings.ts
+++ src/typings.ts
@@ -1,1 +1,36 @@
 export type RequestedControlEvent = "maximize" | "minimize" | "close";
+export type ApplicationWindowTheme = "fluent-light-purple";
+export type ApplicationSettingsStatus = "failed" | "ok";
+
+export interface Resolution {
+	readonly width: number;
+	readonly height: number;
+}
+
+export interface ReadSettingsFail {
+	readonly reason: string;
+	readonly error: Error;
+	readonly status: ApplicationSettingsStatus;
+}
+
+export interface ApplicationWindowSettings {
+	readonly resolution: Resolution;
+	readonly theme: ApplicationWindowTheme;
+}
+
+export interface ApplicationPathSettings {
+	readonly appDataPath: string | null;
+	readonly downloadPath: string | null;
+	readonly ffmpegPath: string | null;
+}
+
+export interface ApplicationServerSettings {
+	readonly port: number;
+}
+
+export interface ApplicationSettings {
+	readonly window: ApplicationWindowSettings;
+	readonly path: ApplicationPathSettings;
+	readonly server: ApplicationServerSettings;
+	readonly status: ApplicationSettingsStatus;
+}
